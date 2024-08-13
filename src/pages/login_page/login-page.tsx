0fbd12830a1c707a48f304b8dea99a3c4e87624e
brian321 <brianperezdota@gmail.com>
2024-08-13 17:54:03 -0400

    add login page with supabase

--- src/pages/login_page/login-page.tsx
+++ src/pages/login_page/login-page.tsx
@@ -1,15 +1,14 @@
-
 import { useState, useEffect } from 'react'
 import { createClient, Session } from '@supabase/supabase-js'
 import { Auth } from '@supabase/auth-ui-react'
 import { ThemeSupa } from '@supabase/auth-ui-shared'
 
-
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ANON_KEY);
+  import.meta.env.VITE_SUPABASE_ANON_KEY
+);
 
-export default function LoginPage () {
+export default function LoginPage() {
   const [session, setSession] = useState<Session | null>(null)
 
   useEffect(() => {
@@ -27,9 +26,16 @@ export default function LoginPage () {
   }, [])
 
   if (!session) {
-    return (<Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />)
-  }
-  else {
+    return (
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={['google', 'github']} // Incluye solo los proveedores deseados
+        dark= {true}
+        theme='dark'
+      />
+    )
+  } else {
     return (<div>Logged in!</div>)
   }
 }
